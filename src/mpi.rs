7fda15dd8123ef4f24aaa9e6941fe7144a091ba6
Chris West (Faux) <git@goeswhere.com>
2017-11-13 18:04:52 +0000

    compare numbers as numbers, not as arrays

--- src/mpi.rs
+++ src/mpi.rs
@@ -1,30 +1,36 @@
-use num_bigint::BigUint;
+use num::bigint::BigUint;
+use num::integer::Integer;
 
 #[inline]
-pub fn pow_mod(val: &[u8], exp: &[u8], modulus: &[u8]) -> Vec<u8> {
-    pre_validate_number(val);
-    pre_validate_number(exp);
-    pre_validate_number(modulus);
-    BigUint::from_bytes_be(val)
-        .modpow(
-            &BigUint::from_bytes_be(exp),
-            &BigUint::from_bytes_be(modulus),
-        )
-        .to_bytes_be()
+pub fn pow_mod(val: &[u8], exp: &[u8], modulus: &[u8]) -> BigUint {
+    let modulus = BigUint::from_bytes_be(modulus);
+    let exp = BigUint::from_bytes_be(exp);
+
+    assert!(modulus.is_odd());
+    assert!(exp.is_odd());
+
+    BigUint::from_bytes_be(val).modpow(&exp, &modulus)
 }
 
 #[inline]
-fn pre_validate_number(val: &[u8]) {
-    assert!(!val.is_empty(), "no bytes is no number");
-    assert!(val.len() > 1 || val[0] != 0, "zero is not okay");
+pub fn eq(left: &BigUint, right: &[u8]) -> bool {
+    BigUint::from_bytes_be(right).eq(left)
 }
 
 #[cfg(test)]
 mod tests {
+    use num::Zero;
+    use num::bigint::BigUint;
+
     #[test]
     fn powm() {
-        assert_eq!(&[9], super::pow_mod(&[7], &[2], &[40]).as_slice());
-        // (259^3) % 512 == 283
-        assert_eq!(&[1, 27], super::pow_mod(&[1, 3], &[3], &[2, 0]).as_slice());
+        assert_eq!(&[125], super::pow_mod(&[5], &[3], &[255]).to_bytes_be().as_slice());
+        // (259^3) % 513 == 283
+        assert_eq!(&[1, 211], super::pow_mod(&[1, 7], &[3], &[2, 1]).to_bytes_be().as_slice());
+    }
+
+    #[test]
+    fn empty_vec() {
+        assert!(BigUint::from_bytes_be(&[]).is_zero());
     }
 }
--- src/rsa.rs
+++ src/rsa.rs
@@ -3,14 +3,9 @@ use errors::*;
 use mpi;
 
 pub fn verify(sig: &[u8], (n, e): (&[u8], &[u8]), padded_hash: &[u8]) -> Result<()> {
-    let mut expected = mpi::pow_mod(sig, &e, &n);
+    let expected = mpi::pow_mod(sig, &e, &n);
 
-    // Horribly inefficient, unless the compiler fixes it. But, also, should only be one byte.
-    while expected.len() < padded_hash.len() {
-        expected.insert(0, 0);
-    }
-
-    if expected == padded_hash {
+    if mpi::eq(&expected, padded_hash) {
         return Ok(());
     }
 
