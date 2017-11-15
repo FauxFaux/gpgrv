1ef9c0b58470aaf503a2f23e6886cac3f1d2f87d
Chris West (Faux) <git@goeswhere.com>
2017-11-15 18:29:45 +0000

    nope, that seems 'reasonable'

--- src/hash_multimap.rs
+++ src/hash_multimap.rs
@@ -25,38 +25,38 @@ impl<K: Eq + Hash, V: Eq + Hash> HashMultiMap<K, V> {
 
     pub fn entries(&self) -> Entries<K, V> {
         Entries {
-            inner: self,
-            top_iter: self.inner.iter(),
-            bot_iter: None,
+            outer_iter: self.inner.iter(),
+            inner_iter: None,
         }
     }
 }
 
 pub struct Entries<'h, K: 'h, V: 'h> {
-    inner: &'h HashMultiMap<K, V>,
-    top_iter: hash_map::Iter<'h, K, HashSet<V>>,
-    bot_iter: Option<(&'h K, hash_set::Iter<'h, V>)>,
+    outer_iter: hash_map::Iter<'h, K, HashSet<V>>,
+    inner_iter: Option<(&'h K, hash_set::Iter<'h, V>)>,
 }
 
 impl<'h, K, V: Eq + Hash> Iterator for Entries<'h, K, V> {
     type Item = (&'h K, &'h V);
 
+    // This looks terrible, I must be being dumb...
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if self.bot_iter.is_none() {
-                self.bot_iter = match self.top_iter.next() {
+            if self.inner_iter.is_none() {
+                self.inner_iter = match self.outer_iter.next() {
                     Some((k, v)) => Some((k, v.iter())),
                     None => return None,
                 };
             }
 
-            if let Some((k, ref mut it)) = self.bot_iter {
+            {
+                let &mut (k, ref mut it) = self.inner_iter.as_mut().unwrap();
                 if let Some(v) = it.next() {
                     return Some((k, v));
                 }
             }
 
-            self.bot_iter = None;
+            self.inner_iter = None;
         }
     }
 }
@@ -73,7 +73,11 @@ mod tests {
         assert!(!map.insert(6, 8));
 
         assert_eq!(3, map.entries().count());
-        assert_eq!(vec![(5, 2), (6, 7), (6, 8)].into_iter().collect::<HashSet<(u64, u64)>>(),
-                   map.entries().map(|(k, v)| (*k, *v)).collect());
+        assert_eq!(
+            vec![(5, 2), (6, 7), (6, 8)]
+                .into_iter()
+                .collect::<HashSet<(u64, u64)>>(),
+            map.entries().map(|(k, v)| (*k, *v)).collect()
+        );
     }
 }
