948021759cbbf4a8efabc22e2ffc957e6a862fad
Chris West (Faux) <git@goeswhere.com>
2017-11-16 09:40:23 +0000

    reject < 2048 bit signatures

--- src/rsa.rs
+++ src/rsa.rs
@@ -3,11 +3,15 @@ use errors::*;
 use mpi;
 
 pub fn verify(sig: &[u8], (n, e): (&[u8], &[u8]), padded_hash: &[u8]) -> Result<()> {
+    if sig.len() < (2048 / 8) {
+        bail!("signature too short");
+    }
+
     let expected = mpi::pow_mod(sig, e, n);
 
     if mpi::eq(&expected, padded_hash) {
         return Ok(());
     }
 
-    bail!("signatures don't match");
+    Err("signatures don't match".into())
 }
