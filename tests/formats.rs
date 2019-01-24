b9ead19dfb774091ca066d0dbcc6ddd658c96ab5
Chris West (Faux) <git@goeswhere.com>
2019-01-24 21:59:38 +0000

    check inline data

--- tests/formats.rs
+++ tests/formats.rs
@@ -9,26 +9,37 @@ use failure::ResultExt;
 use gpgrv::Keyring;
 
 const FAUX_KEY: &[u8] = include_bytes!("faux.pubkey");
-const INPUT_TXT: &str = include_str!("formats/input.txt");
+const INPUT_TXT: &[u8] = include_bytes!("formats/input.txt");
 const INPUT_DAT: &[u8] = include_bytes!("formats/input.dat");
 
 #[test]
 fn load() -> Result<(), Error> {
     #[rustfmt::skip]
     let files = &[
-        ("dat detach armour ", &include_bytes!("formats/output.dat.detach-armour")[..]),
-        ("dat detach binary ", &include_bytes!("formats/output.dat.detach-binary")[..]),
-        ("dat inline armour ", &include_bytes!("formats/output.dat.inline-armour")[..]),
-        // ("dat inline binary ", &include_bytes!("formats/output.dat.inline-binary")[..]),
-        ("txt detach armour ", &include_bytes!("formats/output.txt.detach-armour")[..]),
-        ("txt detach binary ", &include_bytes!("formats/output.txt.detach-binary")[..]),
-        ("txt inline armour ", &include_bytes!("formats/output.txt.inline-armour")[..]),
-        // ("txt inline binary ", &include_bytes!("formats/output.txt.inline-binary")[..]),
+        ("dat", "detach armour ", &include_bytes!("formats/output.dat.detach-armour")[..]),
+        ("dat", "detach binary ", &include_bytes!("formats/output.dat.detach-binary")[..]),
+        ("dat", "inline armour ", &include_bytes!("formats/output.dat.inline-armour")[..]),
+        // ("dat", "inline binary ", &include_bytes!("formats/output.dat.inline-binary")[..]),
+        ("txt", "detach armour ", &include_bytes!("formats/output.txt.detach-armour")[..]),
+        ("txt", "detach binary ", &include_bytes!("formats/output.txt.detach-binary")[..]),
+        ("txt", "inline armour ", &include_bytes!("formats/output.txt.inline-armour")[..]),
+        // ("txt", "inline binary ", &include_bytes!("formats/output.txt.inline-binary")[..]),
     ];
 
-    for (name, file) in files {
-        gpgrv::read_doc(io::Cursor::new(file), iowrap::Ignore::new())
-            .with_context(|_| format_err!("reading {}", name))?;
+    for (input, name, file) in files {
+        let mut out = Vec::with_capacity(8096);
+        gpgrv::read_doc(io::Cursor::new(file), &mut out)
+            .with_context(|_| format_err!("reading {} {}", input, name))?;
+
+        if name.starts_with("inline") {
+            assert_eq!(out, match *input {
+                "dat" => &INPUT_DAT[..INPUT_DAT.len() - 1],
+                "txt" => &INPUT_TXT[..INPUT_TXT.len() - 1],
+                _ => unreachable!(),
+            }, "checking {} {}", input, name);
+        } else {
+            assert!(out.is_empty(), "detached has no data: {} {}", input, name);
+        }
     }
 
     Ok(())
@@ -45,7 +56,7 @@ fn inline_armour() -> Result<(), Error> {
             &mut out,
             &keyring,
         )?;
-        assert_eq!(out, INPUT_TXT.trim().as_bytes());
+        assert_eq!(out, String::from_utf8(INPUT_TXT.to_vec())?.trim().as_bytes());
     }
     #[cfg(todo)]
     {
