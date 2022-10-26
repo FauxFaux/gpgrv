49154ed480809fb0ec2150f86851b5616b2ec00a
Chris West (Faux) <git@goeswhere.com>
2022-10-26 20:00:44 +0100

    feat: bump deps

--- examples/gpgv.rs
+++ examples/gpgv.rs
@@ -1,34 +1,32 @@
 use std::fs;
 use std::io;
 
-use anyhow::anyhow;
-use anyhow::Context;
-use anyhow::Error;
-use clap::App;
-use clap::Arg;
+use anyhow::{anyhow, Context, Error, Result};
+use clap::{Arg, ArgAction};
 
-fn main() -> Result<(), Error> {
-    let matches = App::new("gpgv")
+fn main() -> Result<()> {
+    let matches = clap::command!()
         .arg(
-            Arg::with_name("keyring")
+            Arg::new("keyring")
                 .long("keyring")
                 .value_name("FILE")
-                .multiple(true)
+                .num_args(1)
                 .required(true)
-                .number_of_values(1)
+                .action(ArgAction::Append)
                 .help("take the keys from the keyring FILE"),
         )
         .arg(
-            Arg::with_name("FILES")
-                .multiple(true)
+            Arg::new("FILES")
                 .required(true)
+                .num_args(1)
+                .action(ArgAction::Append)
                 .index(1)
                 .help("files to verify"),
         )
         .get_matches();
 
     let mut keyring = gpgrv::Keyring::new();
-    for path in matches.values_of_os("keyring").unwrap() {
+    for path in matches.get_raw("keyring").expect("required arg") {
         keyring
             .append_keys_from(
                 fs::File::open(path).with_context(|| anyhow!("opening keyring {:?}", path))?,
@@ -40,7 +38,7 @@ fn main() -> Result<(), Error> {
         println!("loaded key: {:016x}", key);
     }
 
-    for file in matches.values_of_os("FILES").unwrap() {
+    for file in matches.get_raw("FILES").expect("required arg") {
         gpgrv::verify_message(
             io::BufReader::new(
                 fs::File::open(file).with_context(|| anyhow!("opening input file {:?}", file))?,
